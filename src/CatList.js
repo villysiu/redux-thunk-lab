61181f899443702097aacb0e666668a14003fec8
villysiu <villysiu@gmail.com>
2021-09-02 15:04:19 -0700

    commit tree 49a5d9b4834f63cb8706f1c134be6419e088a645

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,18 @@
 // write your CatList component here
+import React, { Component } from 'react';
+
+class CatList extends Component {
+    render() {
+        return (
+            <div>
+                { 
+                    this.props.catPics.map((cat) => 
+                    <div key={cat.id}><img src={cat.url} alt=""/></div>)
+                }
+            
+            </div>
+        )
+    }
+
+}
+export default CatList;
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,15 @@
 // write and export your action creator function here
+// Reducer is expecting { type: 'ADD_CATS', cats: [ cat data from API ]]}
+
+export const fetchCats = () => {
+    return (dispatch) => {
+      dispatch({ type: "LOADING_CATS" });
+      fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+        .then((response) => {
+          return response.json();
+        })
+        .then((responseJSON) => {
+          dispatch({ type: "ADD_CATS", cats: responseJSON.images });
+        });
+    };
+  };
